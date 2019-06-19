7c12d7503d29be6b842050e32434a3d2f9a91b5f
Zac Cornell <zaccornell.design@gmail.com>
2019-06-19 15:58:02 +1000

    fixed game

--- Assets/Scripts/TriggerNote.cs
+++ Assets/Scripts/TriggerNote.cs
@@ -9,11 +9,17 @@ public class TriggerNote : MonoBehaviour
     [SerializeField] KeyCode hitButton = KeyCode.Space;
     [SerializeField] UnityEvent OnHit;
 
+    //private void Update()
+    //{
+    //    Debug.Log("Updating...");
+    //}
+
     void OnTriggerStay(Collider col)
     {
+
         var hit = col.GetComponent<Note>();
 
-        Debug.Log("Note hit: " + hit);
+       
 
         if (hit is Note)
         {
@@ -22,7 +28,7 @@ public class TriggerNote : MonoBehaviour
                 //Successful hit!
                 OnHit.Invoke();
 
-                Debug.Log("HIT!");
+               
 
                 //Increase score
                 ScoreManager.instance.AddScore(10);
@@ -36,5 +42,7 @@ public class TriggerNote : MonoBehaviour
 
         }
     }
-    
+
+   
+
 }
