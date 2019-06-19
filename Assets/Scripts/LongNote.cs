5f04aac1596a77db890f1345723cf0174e0c0ac5
Zac Cornell <zaccornell.design@gmail.com>
2019-06-20 09:11:09 +1000

    added long notes

--- Assets/Scripts/LongNote.cs
+++ Assets/Scripts/LongNote.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LongNote : MonoBehaviour
+public class LongNote : Note
 {
     //[SerializeField] Material mat;
-    public int score = 20;
+    //public int score = 20;
 
-    private void Start()
-    {
-        //if (mat != null)
-        //{
-        //    var renderer = GetComponent<MeshRenderer>();
-        //    renderer.material = mat;
-        //}
-    }
+    //private void Start()
+    //{
+    //    //if (mat != null)
+    //    //{
+    //    //    var renderer = GetComponent<MeshRenderer>();
+    //    //    renderer.material = mat;
+    //    //}
+    //}
 }
